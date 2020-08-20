770c5f752d906a6e00204ca1777ff10faf2d66ac
DESKTOP-TDIBDEL\macknae <dusk0364@gmail.com>
2020-08-20 18:14:20 +0900

    $git commit -m "commit 설명"

---
+++ src/main/java/kr/co/yeonayeo/Profile.java
@@ -0,0 +1,22 @@
+package kr.co.yeonayeo;
+
+public class Profile {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
