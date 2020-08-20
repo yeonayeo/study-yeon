93329a438d9245f29a5b916876af862a709d85b0
DESKTOP-TDIBDEL\macknae <dusk0364@gmail.com>
2020-08-20 18:01:13 +0900

    $git commit -m "commit 설명"

---
+++ src/main/java/kr/co/yeonayeo/StudyApiApplication.java
@@ -0,0 +1,11 @@
+package kr.co.yeonayeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApiApplication.class, args);
+    }
+}
